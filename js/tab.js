fea7550e80060dadd44e79a7363ebbdbb1e5e3e5
mark <h091237557@gmail.com>
2015-04-17 23:00:51 +0800

    add getFrends and getMe

---
+++ js/tab.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+
+    $('.tabs .tab-links a').on('click',function(e){
+	var currentAttrValue =$(this).attr('href');
+	$('.tabs ' + currentAttrValue).siblings().slideUp(400);
+	$('.tabs ' + currentAttrValue).delay(400).slideDown(400);
+
+	$(this).parent('li').addClass('active').siblings().removeClass('active');
+	e.preventDefault();
+
+
+    });
+});
+
+
