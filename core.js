115d1e2f1dec3bf05782f3c1ed761e15e2c10c70
JackChan <jack5241027@gmail.com>
2015-04-28 23:59:59 +0800

    先依ＦＢ官網走

--- core.js
+++ core.js
@@ -1,93 +1,84 @@
+(function() {
 
+    // var appId = '1052783271416275';
+    // var url = 'http://localhost/FbApiTest/index.html';
 
-(function(){
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId: '609847775785632',
+            xfbml: true,
+            version: 'v2.3'
+        });
+        
+    };
 
-    var appId = '1052783271416275';
-    var url = 'http://localhost/FbApiTest/index.html';
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
-    FB.init({
-	appId:appId,
-	xfbml:true,
-	version:'v2.3'
-    });
+    function getMeFriends() {
+        //taggable_friends
+        FB.api('/me/taggable_friends', function(response) {
+            var htmlData = "";
+            var jsonData = "";
+            var limit = 40;
+            var count = 0;
+            for (var key in response.data) {
+                if (count <= limit) {
+                    var friend = response.data[key];
+                    var friend_name = friend.name;
+                    var friend_image = friend.picture.data.url;
 
+                    htmlData += "<li class='friend-item'>";
+                    htmlData += "<img src='" + friend_image + "'>";
+                    htmlData += "<a>" + friend_name + "</a>";
+                    htmlData += "</li>";
+                    count++;
+                }
 
-    FB.getLoginStatus(function(response){
-	if(response.status === 'connected'){
-	    var uid = response.authResponse.userID;
-	    getMeFriends();
-	    getMe();
-	    getMeFeed();
-	}else{
-	    window.top.location.href = "http://www.facebook.com/connect/uiserver.php?app_id="
-	+ encodeURIComponent(appId)
-	+ "&next="
-	+ encodeURIComponent(url)
-	+
-	"&display=popup&perms=email,user_friends,user_birthday,user_likes&fbconnect=1&method=permissions.request";
-	}
-    });
+            }
+            $('.friends-list').html(htmlData);
+            // $(".friends-list").html(jsonData.stringify());
 
-    function getMeFriends(){
-	//taggable_friends
-	FB.api('/me/taggable_friends', function(response) {
-	    var htmlData ="";
-	    var jsonData ="";
-	    var limit = 40;
-	    var count =0;
-	    for (var key in response.data){
-		if (count <= limit){
-		    var friend = response.data[key];
-		    var friend_name = friend.name;
-		    var friend_image = friend.picture.data.url;
-
-		    htmlData += "<li class='friend-item'>";
-		    htmlData += "<img src='" + friend_image +"'>";
-		    htmlData += "<a>" + friend_name + "</a>";
-		    htmlData += "</li>";
-		    count ++;
-		}
-
-	    }
-	    $('.friends-list').html(htmlData);	
-	   // $(".friends-list").html(jsonData.stringify());
-
-	});
+        });
 
     }
-    function getMe(){
-	FB.api('/me', function(response) {
-	    var htmlData ="";
-	    for (var key in response){
-		htmlData += "<pre>";
-		htmlData +=  key + " : " + response[key];
-		htmlData += "</pre>";
-	    }
-	    $('#tab2').html(htmlData);
-	     
-	});
+
+    function getMe() {
+        FB.api('/me', function(response) {
+            var htmlData = "";
+            for (var key in response) {
+                htmlData += "<pre>";
+                htmlData += key + " : " + response[key];
+                htmlData += "</pre>";
+            }
+            $('#tab2').html(htmlData);
+
+        });
     }
 
-    function getMeFeed(){
-	FB.api('/me/Feed',function(response){
-	    var htmlData= "";	
-	    for (var key in response.data){
-               var feed = response.data[key];
-	       var picture = feed.picture;
-	       var story = feed.story;
-	       var createTime = feed.created_time;
-	       var comments = feed.comments.data;
+    function getMeFeed() {
+        FB.api('/me/Feed', function(response) {
+            var htmlData = "";
+            for (var key in response.data) {
+                var feed = response.data[key];
+                var picture = feed.picture;
+                var story = feed.story;
+                var createTime = feed.created_time;
+                var comments = feed.comments.data;
 
-	    }	    
-	});
+            }
+        });
 
     }
 
 
 
-})(); 
-
-
-
-
-
+})();
